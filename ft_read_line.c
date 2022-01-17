0f2b84f8abecee7c7e357ff263b66677a119eeda
Andera Bridger <abridger@oa-f5.msk.21-school.ru>
2022-01-17 13:13:05 +0300

    testing some functions of project

---
+++ ft_read_line.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_read_line.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abridger <abridger@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 18:03:08 by abridger          #+#    #+#             */
+/*   Updated: 2022/01/15 18:27:18 by abridger         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_test_readline(void)
+{
+	char	*input;
+	int		size;
+	int		i;
+	// char	cwd[100];
+	char	*buf;
+
+	buf = NULL;
+	i = 0;
+	while (i < 5)
+	{
+		input = readline("Minishell$ : ");
+		size = ft_strlen(input);
+		if (!strcmp(input, "pwd"))
+			printf("%s", getcwd(NULL, sizeof(buf)));
+		else
+		{
+			add_history(input);
+			printf("%s", input);
+			printf("\n");
+			printf("%d", size);
+		}
+		printf("\n");
+		++i;
+		if (input)
+			free(input);
+	}
+}
+
+void	ft_print_lsts(t_data *data)
+{
+	if (data->envrmnt != NULL)
+	{
+		while (data->envrmnt)
+		{
+			printf("%s\n", data->envrmnt->line);
+			data->envrmnt = data->envrmnt->next;
+		}
+	}
+	else
+		printf("test");
+}
